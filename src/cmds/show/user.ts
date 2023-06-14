c390044ce86cb069f1e23cea167877745dd767ec
Aaron <aaronli5260@gmail.com>
2023-06-14 16:58:15 -0500

    pseudo code logic

--- src/cmds/show/user.ts
+++ src/cmds/show/user.ts
@@ -1,13 +1,20 @@
-import { logSuccess } from '../../logger'
+import { log } from '../../logger'
 
 const CMD = 'user'
 
 function addUserCmd(cmd) {
-    cmd.command(CMD).action(showLocation)
+    cmd.command(CMD).action(showUser)
 }
 
-async function showLocation() {
-    logSuccess('seeing this route')
+/**
+ * Show the current user.
+ */
+async function showUser() {
+    // if user logged in
+    // log(username)
+    // else
+    // log(No current user is set. Try running "spec login")
+    log('No current user is set. Try running "spec login"')
 }
 
 export default addUserCmd
