de90733f29d56f75e85ec331d6f8091d14675149
Ben Whittle <benwhittle31@gmail.com>
2023-08-29 20:53:20 -0500

    upgrade core-lib

--- src/files/testLiveObject.ts
+++ src/files/testLiveObject.ts
@@ -12,7 +12,7 @@ import {
     TableSpec,
     ColumnSpec,
     BigInt,
-} from 'https://esm.sh/@spec.dev/core@0.0.106'
+} from 'https://esm.sh/@spec.dev/core@0.0.107'
 import { createEventClient, SpecEventClient } from 'https://esm.sh/@spec.dev/event-client@0.0.16'
 import {
     buildSelectQuery,
