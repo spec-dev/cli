d051d03b4283d531f7a1f65da9ba363c6a0e1dd0
Ben Whittle <benwhittle31@gmail.com>
2023-10-28 18:51:29 -0500

    bump core version

--- src/files/testLiveObject.ts
+++ src/files/testLiveObject.ts
@@ -12,7 +12,7 @@ import {
     TableSpec,
     ColumnSpec,
     BigInt,
-} from 'https://esm.sh/@spec.dev/core@0.0.131'
+} from 'https://esm.sh/@spec.dev/core@0.0.132'
 import { createEventClient, SpecEventClient } from 'https://esm.sh/@spec.dev/event-client@0.0.16'
 import {
     buildSelectQuery,
